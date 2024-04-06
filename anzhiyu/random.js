6106e7d701522c8721e804425be71a40085be15d
季同学 <2840724950@qq.com>
2024-04-06 20:19:07 +0800

    Site updated: 2024-04-06 20:19:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/05/hello-world/","2024/04/06/景德镇陶瓷抢摊位老太婆打人事件/"];function toRandomPost(){
+var posts=["2024/04/06/景德镇陶瓷抢摊位老太婆打人事件/","2024/04/05/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
